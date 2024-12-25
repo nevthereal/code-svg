1ec868746f1841f27e991d8f04f34bebd5eb00f4
Neville Brem <bremneville@gmail.com>
2024-12-25 09:43:18 +0100

    removed unneccessary code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
 	import { codeToHtml, type BuiltinLanguage } from 'shiki';
-	import { toPng, toSvg } from 'html-to-image';
 
-	let code = $state(`{#await data.posts}
-	<p class="italic font-mono">Loading posts ...</p>
-{:then posts}
-	<div class="flex flex-col gap-4">
-		{#each posts as post}
-			<Post {post} currentUserId={userId} />
-		{/each}
-	</div>
-{/await}`);
+	let code = $state('');
 	let lang = $state('typescript');
 
 	const htmlPromise = $derived(
